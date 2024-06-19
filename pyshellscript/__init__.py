7097325eeccd01c879d3c7e7ff1c0cda02392dd7
heX <hex1625616@gmail.com>
2024-06-19 17:47:27 +0200

    Fixed __init__ (?) - there was a problem that the `__name__` variable in the previous solution changed its value to invalid value

--- pyshellscript/__init__.py
+++ pyshellscript/__init__.py
@@ -1,6 +1,1 @@
 from .pyshellscript import *
-
-# I don't know why, but without this lines the `import *` doesn't work
-# (although theoretically it should work)
-import pyshellscript
-__all__ = dir(pyshellscript)
