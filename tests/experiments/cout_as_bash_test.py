2459ed5451a7101b68e5c91962c68c972c97e726
heX <hex1625616@gmail.com>
2024-09-12 17:56:19 +0200

    upd

--- tests/experiments/cout_as_bash_test.py
+++ tests/experiments/cout_as_bash_test.py
@@ -1,13 +1,39 @@
 import subprocess
 
 class ShellCommand:
+    """
+    A class that represents a shell command. It allows chaining of commands
+    using pipes and captures the output for further processing.
+
+    Attributes:
+        command (str): The shell command to be executed.
+        process (subprocess.Popen): The process object representing the running command.
+        output (str): The output of the command after execution.
+    """
+
     def __init__(self, command):
+        """
+        Initializes the ShellCommand instance with the specified command.
+
+        Args:
+            command (str): The shell command to execute.
+        """
         self.command = command
         self.process = None
         self.output = None
 
     def run(self, input_data=None):
-        # Выполняем команду с возможностью передачи входных данных через PIPE
+        """
+        Executes the command. If input_data is provided, it is passed to the
+        command via stdin.
+
+        Args:
+            input_data (str, optional): The input data to pass to the command.
+
+        Returns:
+            ShellCommand: Returns self to allow method chaining.
+        """
+        # Execute the command with input data passed via PIPE if provided
         if input_data is not None:
             self.process = subprocess.Popen(self.command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
             self.output, _ = self.process.communicate(input=input_data)
@@ -18,33 +44,75 @@ class ShellCommand:
         return self
 
     def __rshift__(self, other):
-        # Убедимся, что команда выполнена, если не было выполнено до этого
+        """
+        Overloads the '>>' operator to allow piping of one command's output
+        into another command or saving it to a file.
+
+        Args:
+            other (ShellCommand or SaveToFile): The next command to run or
+            the object to save the output to.
+
+        Returns:
+            ShellCommand or SaveToFile: The resulting object after piping.
+        """
+        # Ensure the command has been executed if not already
         if self.output is None:
             self.run()
 
-        # Передаем вывод следующей команде
+        # If the next object is a ShellCommand, pipe the output to it
         if isinstance(other, ShellCommand):
             return other.run(input_data=self.output)
+        # If the next object is SaveToFile, write the output to the file
         elif isinstance(other, SaveToFile):
             other.write_to_file(self.output)
             return other
 
+
 class SaveToFile:
+    """
+    A class that represents a file-saving operation. It allows the output of
+    commands to be saved to a specified file.
+
+    Attributes:
+        filename (str): The name of the file to which the output will be saved.
+    """
+
     def __init__(self, filename):
+        """
+        Initializes the SaveToFile instance with the specified filename.
+
+        Args:
+            filename (str): The name of the file to save the output to.
+        """
         self.filename = filename
 
     def write_to_file(self, data):
+        """
+        Writes the given data to the file.
+
+        Args:
+            data (str): The data to write to the file.
+        """
         with open(self.filename, 'w') as file:
             file.write(data)
 
     def __rshift__(self, other):
-        # Для возможности продолжать цепочку после сохранения файла
+        """
+        Overloads the '>>' operator to allow chaining after saving to a file.
+
+        Args:
+            other (any): The next operation or object to process after saving.
+
+        Returns:
+            any: The resulting object after chaining.
+        """
         return other
 
 
-# Пример использования
+# Example usage
 sh = ShellCommand
 save_to_file = SaveToFile
 
+# Test command:
 sh('ping 127.0.0.1') >> sh('find "Reply"') >> save_to_file('log.txt')
 #sh('ping 127.0.0.1') >> save_to_file('log.txt')
