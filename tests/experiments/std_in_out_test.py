2ea0936ac18816bab738744f7865a18a21139d63
heX <hex1625616@gmail.com>
2024-10-09 00:48:28 +0200

    Experiments: str pipe test `StdInOutPipeProcess([StdInOutRemoveControlChars(), ...`

--- tests/experiments/std_in_out_test.py
+++ tests/experiments/std_in_out_test.py
@@ -1,24 +1,19 @@
-class StdInOutBase:
-    def __init__(self, next_input=None):
-        self.next_input = next_input
 
-    def input(self, s: str):
+class StdInOutBase:
+    def input(self, s: str) -> str:
         if self.next_input is None:
             self.output(s)
         else:
             self.next_input.input(s)
 
-    def output(self, s: str):
-        raise NotImplementedError("Subclasses should implement this method")
-
 
 class StdInOutLinesUpper(StdInOutBase):
-    def input(self, s: str):
-        super().input(s.upper())
+    def input(self, s: str) -> str:
+        return s.upper()
 
 
 class StdInOutLinesPrint(StdInOutBase):
-    def output(self, s: str):
+    def input(self, s: str) -> None:
         print(s)
 
 
@@ -26,32 +21,42 @@ class StdInOutRemoveControlChars(StdInOutBase):
     _delete_chars = None
     _trans_table = None
 
-    def __init__(self, next_input=None):
-        super().__init__(next_input)
+    def __init__(self, ):
+        super().__init__()
         if StdInOutRemoveControlChars._delete_chars is None:
             StdInOutRemoveControlChars._delete_chars = ''.join([chr(i) for i in range(32) if i not in (9, 10, 13)])
         if StdInOutRemoveControlChars._trans_table is None:
             StdInOutRemoveControlChars._trans_table = str.maketrans('', '', StdInOutRemoveControlChars._delete_chars)
 
-    def input(self, s: str):
-        filtered_str = s.translate(self._trans_table)
-        super().input(filtered_str)
+    def input(self, s: str) -> str:
+        return s.translate(self._trans_table)
 
 
 class StdInOutReplaceTabs(StdInOutBase):
-    def __init__(self, next_input=None, spaces_per_tab=4):
-        super().__init__(next_input)
+    def __init__(self, spaces_per_tab=4):
+        super().__init__()
         self.spaces_per_tab = spaces_per_tab
 
-    def input(self, s: str):
-        replaced_str = s.replace('\t', ' ' * self.spaces_per_tab)
-        super().input(replaced_str)
+    def input(self, s: str) -> str:
+        return s.replace('\t', ' ' * self.spaces_per_tab)
 
+
+class StdInOutPipeProcess(StdInOutBase):
+    def __init__(self, pipe_processes):
+        self.pipe_processes = pipe_processes
+
+    def input(self, s: str) -> str:
+        for proc in self.pipe_processes:
+            s = proc.input(s)
+        return s
+
+def str_pipe_process(pipe_procs, s: str):
+    for proc in pipe_procs:
+        s = proc.input(s)
 
 
 # Example usage:
-#process1 = StdInOutRemoveControlChars(StdInOutLinesUpper(StdInOutLinesPrint()))
-process1 = StdInOutRemoveControlChars(StdInOutReplaceTabs(StdInOutLinesUpper(StdInOutLinesPrint())))
+process1_pipe_obj = StdInOutPipeProcess([StdInOutRemoveControlChars(), StdInOutReplaceTabs(8), StdInOutLinesUpper(), StdInOutLinesPrint()])
 
-process1.input('Hello, world!\x01\x02\x03')
-process1.input('Hello, world!\n\r_test_\x01\x02\t\t\x00\x03_test_!')
+process1_pipe_obj.input('Hello, world!\x01\x02\x03')
+process1_pipe_obj.input('Hello, world!\n\r_test_\x01\x02\t\x00\x03_test_!')
